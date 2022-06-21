d62b27cf6b33f964eb256132cb9347884c535e6a
chudtmse150303 <chudtmse150303@fpt.edu.vn>
2022-06-21 10:19:24 +0700

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,27 +4,41 @@ import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Component } from 'react';
-import { DISHES } from '../shared/dish';
+import { DISHES } from '../shared/dish2';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+import Contact from './ContactComponent';
 class Main extends Component {
 
 
     constructor(props) {
         super(props);
         this.state = {
-            dish: DISHES,
-            // selectedDish: null
+            dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
         }
     }
-    // onDishSelect(dishId) {
-    //     this.setState({ selectedDish: dishId });
-    // }
+
+    HomePage = () => {
+        return (
+            <Home
+                dish={this.state.dishes.filter(dish => dish.featured)[0]}
+                promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                leader={this.state.leaders.filter(leader => leader.featured)[0]}
+            />
+        )
+    }
     render() {
         return (
             <>
                 <Header />
                 <Routes>
-                    <Route path='/home' element={<Home />} ></Route>
-                    <Route exact path='/menu' element={<Menu dishes={this.state.dish}    />}></Route>
+                    <Route path='/home' element={<this.HomePage />} ></Route>
+                    <Route exact path='/menu' element={<Menu dishes={this.state.dishes} />}></Route>
+                    <Route exact path='/contactus' element={<Contact />}></Route>
                     <Route path="*" element={<Navigate to="/menu" />} />
                 </Routes>
 
